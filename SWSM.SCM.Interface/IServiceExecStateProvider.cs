ad65891f64c9d63d29b1d5e4327515cff5509378
SOCAR <socar@socar.pl>
2025-09-05 23:55:49 +0200

    minor comments

--- SWSM.SCM.Interface/IServiceExecStateProvider.cs
+++ SWSM.SCM.Interface/IServiceExecStateProvider.cs
@@ -2,17 +2,37 @@
 
 namespace SWSM.SCM.Interface
 {
+    /// <summary>
+    /// Provides methods to control the execution state of a service, such as starting, stopping, pausing, and continuing.
+    /// </summary>
     public interface IServiceExecStateProvider
     {
-
+        /// <summary>
+        /// Starts the specified service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service to start.</param>
+        /// <returns>An <see cref="OperationResult"/> indicating the outcome of the operation.</returns>
         OperationResult StartService(string ServiceName);
+
+        /// <summary>
+        /// Stops the specified service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service to stop.</param>
+        /// <returns>An <see cref="OperationResult"/> indicating the outcome of the operation.</returns>
         OperationResult StopService(string ServiceName);
 
+        /// <summary>
+        /// Pauses the specified service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service to pause.</param>
+        /// <returns>An <see cref="OperationResult"/> indicating the outcome of the operation.</returns>
         OperationResult PauseService(string ServiceName);
+
+        /// <summary>
+        /// Continues the specified paused service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service to continue.</param>
+        /// <returns>An <see cref="OperationResult"/> indicating the outcome of the operation.</returns>
         OperationResult ContinueService(string ServiceName);
-
-        
-
-
     }
 }
