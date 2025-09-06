fe13f05ce9a66eb0b85943aeadc1b2cd51e1974e
SOCAR <socar@socar.pl>
2025-09-06 15:03:02 +0200

    commenting code

--- SWSM.SCM.Interface/IServiceInfoProvider.cs
+++ SWSM.SCM.Interface/IServiceInfoProvider.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 
 namespace SWSM.SCM.Interface
 {
+    /// <summary>
+    /// Provides methods to retrieve information about a service, such as its execution state and display name.
+    /// </summary>
     public interface IServiceInfoProvider
     {
+        /// <summary>
+        /// Gets the current execution state of the specified service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service.</param>
+        /// <returns>The current <see cref="ServiceExecStatus"/> of the service.</returns>
         ServiceExecStatus GetCurrentExecutionState(string ServiceName);
 
-        OperationResult GetDisplayName(string ServiceName); 
-
+        /// <summary>
+        /// Gets the display name of the specified service.
+        /// </summary>
+        /// <param name="ServiceName">The name of the service.</param>
+        /// <returns>
+        /// An <see cref="OperationResult"/> containing the display name if successful, or an error message if not.
+        /// </returns>
+        OperationResult GetDisplayName(string ServiceName);
     }
 }
